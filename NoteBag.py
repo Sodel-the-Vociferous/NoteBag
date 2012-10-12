1e2b7d724ee33306c2b7415492a59fa0999d27df
Daniel Ralston <Wubbulous@gmail.com>
2012-10-12 00:29:54 -0700

    Update comments and docstrings

--- NoteBag.py
+++ NoteBag.py
@@ -11,6 +11,8 @@ import string
 import subprocess
 import sys # for platform
 
+# Compensate for Python 2.x and 3.x having different module names, and
+# no good way to make the same imports work on both.
 try:
     # Widgets
     from tkinter import (Button, Entry, Frame, Label, Listbox,
@@ -29,10 +31,15 @@ except ImportError:
 if os.name.lower() == "nt":
     import win32process
 
+# A common-denominator between Python 2.x and 3.x
 PICKLE_PROTOCOL = 2
 
 
 def notes_checksum(notes):
+    """
+    Return the hash digest of a list of notes as a string.
+    """
+
     digest = hashlib.sha1()
     for note_name in notes:
         note_name_bytes = note_name.encode('utf-8')
@@ -42,11 +49,23 @@ def notes_checksum(notes):
     return digest.hexdigest()
 
 def save_notes_list(notes, file_path):
+    """
+    Write a list of notes out to a file. Also write a checksum of the
+    notes list, so read_notes_list() can validate what it reads.
+    """
+
     with open(file_path, "wb") as f:
         pickle.dump(notes_checksum(notes), f, PICKLE_PROTOCOL)
         pickle.dump(notes, f, PICKLE_PROTOCOL)
 
 def read_notes_list(file_path):
+    """
+    Read a list of notes back from a file.
+
+    If the checksum from the file doesn't match the checksum of all of
+    the notes that were read from it, raise a ValueError.
+    """
+
     with open(file_path, "rb") as f:
         saved_checksum = pickle.load(f)
         notes = pickle.load(f)
@@ -57,12 +76,21 @@ def read_notes_list(file_path):
     return notes
 
 def sanitize_note_name(note_name):
+    """
+    Very conservatively remove any characters from a note name that
+    might not play nice with a filesystem.
+    """
+
     note_name = note_name.strip()
     def okay_filename_char(c):
         return c.lower() in "abcdefghijklmnopqrstuvwxyz .-_'"
     return "".join(list(filter(okay_filename_char, tuple(note_name))))
 
 def create_skeleton_note(note_name, note_path, template_file_path):
+    """
+    Create a skeleton note document, containing just the note's name.
+    """
+
     with open(template_file_path) as tf:
         template_lines = tf.readlines()
     skeleton_lines = [line.replace("%(NOTE NAME)%", note_name)
@@ -74,10 +102,16 @@ def create_skeleton_note(note_name, note_path, template_file_path):
             f.write(line)
 
 def open_note(note_path, document_editor=None):
+    """
+    Open a note for editing with with a program.
+
+    Use the document_editor arg if available; otherwise, use a command
+    appropriate to the operating system that will open the default
+    program for the file type.
+    """
+
     # Choose the document editor and Popen() settings based on the
-    # operating system. Use the document_editor arg if available;
-    # otherwise, use the operating-system-appropriate command that
-    # will open the default program for the file type.
+    # operating system.
     #
     # This is SUPER UGLY.
     creationflags = 0
@@ -120,19 +154,36 @@ class NoteBag:
 
     ## Back-End Methods
     def notes_list_path(self):
+        """
+        Return the path to the notes list file.
+        """
+
         notes_list_dir = get_called_script_dir()
         return os.path.join(notes_list_dir, self.notes_list_filename)
 
     def template_note_path(self):
+        """
+        Return the path to the template note file.
+        """
+
         notes_list_dir = get_called_script_dir()
         return os.path.join(notes_list_dir, self.note_template_filename)
 
     def note_path(self, note_name):
+        """
+        Return the path to an existing note document.
+        """
+
         note_filename = self.notes[note_name]
         note_path = os.path.join(self.notes_dir, note_filename)
         return note_path
 
     def get_listbox_selected_note_name(self):
+        """
+        Return the note name that is selected in the listbox; if there
+        is no selection, return None.
+        """
+
         selections = self.note_names_listbox.curselection()
         if not selections:
             return None
@@ -141,6 +192,10 @@ class NoteBag:
         return note_name
 
     def load_config(self):
+        """
+        Load a config file, and use it to set config options.
+        """
+
         config = self.config = read_config(CONFIG_FILENAME)
         self.notes_list_filename = config.get("NoteBag", "Notes List File")
         self.notes_dir = config.get("NoteBag", "Notes Directory")
@@ -148,6 +203,10 @@ class NoteBag:
         self.document_editor = config.get("NoteBag", "Document Editor")
 
     def load_notes_list(self):
+        """
+        Load the list of notes.
+        """
+
         # TODO handle exceptions
         notes_list_path = self.notes_list_path()
         if not os.path.isfile(notes_list_path):
@@ -156,10 +215,19 @@ class NoteBag:
             self.notes = read_notes_list(notes_list_path)
 
     def save_notes_list(self):
+        """
+        Save the list of notes.
+        """
+
         save_notes_list(self.notes, self.notes_list_path())
 
     def get_entered_note_name(self):
-        return self.note_name_entry.get().strip()
+        """
+        Get the text that has been entered into the "Note Name" text
+        entry box, with all leading and trailing spaces stripped off.
+        """
+
+        return self.note_name_entry.get().strip("\t ")
 
     def get_note_name_key(self, note_name):
         note_names = self.notes.keys()
@@ -169,6 +237,10 @@ class NoteBag:
         return None
 
     def add_note(self, note_name, note_filename=None):
+        """
+        Add a note document, and save the list of notes.
+        """
+
         note_path = os.path.join(self.notes_dir, note_filename)
         create_skeleton_note(note_name, note_path, self.template_note_path())
 
@@ -176,6 +248,13 @@ class NoteBag:
         self.save_notes_list()
 
     def update_note_names_list(self):
+        """
+        Update the listbox of the existing notes, and the list's
+        label. If there is any text entered into the "Note Name" text
+        entry box, only list note names that contain the entered
+        text. (This is where incremental search happens.)
+        """
+
         search_str = self.get_entered_note_name()
         note_names = self.notes.keys()
 
@@ -202,12 +281,26 @@ class NoteBag:
         self.note_names_label_strvar.set(s)
 
     def open_note(self, note_name):
+        """
+        Open a note for editing.
+        """
+
         note_filename = self.notes[note_name]
         note_path = os.path.join(self.notes_dir, note_filename)
         open_note(note_path, self.document_editor)
 
     ## GUI Callbacks
     def note_name_action_callback(self, *_args, **_kwargs):
+        """
+        A callback to perform an action based on the text in the "Note
+        Name" text entry box.
+
+        If the name of an existing note has been entered into the text
+        box, open the note; if some other text has been entered,
+        create a note with the entered text as a name; if no text has
+        been entered, show a warning dialog box and do nothing.
+        """
+
         note_name = self.get_entered_note_name()
         note_name = sanitize_note_name(note_name)
         if not note_name:
@@ -228,6 +321,12 @@ class NoteBag:
         self.clear_note_name_entry()
 
     def note_name_entry_changed(self, *_args, **_kwargs):
+        """
+        A callback to update the text entry action ("Open"/"Add")
+        button's label, and update the incremental note list search,
+        based on the text in the "Note Name" text entry box.
+        """
+
         self.update_note_names_list()
 
         entered_note_name = self.get_entered_note_name()
@@ -237,9 +336,18 @@ class NoteBag:
             self.note_name_action_strvar.set("Add")
 
     def clear_note_name_entry(self):
+        """
+        Clear the "Note Name" text entry box.
+        """
+
         self.note_name_entry.delete(0, END)
 
     def open_note_from_listbox(self, *_args, **_kwargs):
+        """
+        If a note name has been selected in the note name list, open
+        it; otherwise, show a warning dialog box and do nothing.
+        """
+
         note_name = self.get_listbox_selected_note_name()
         if not note_name:
             # TODO show a warning dialog box or something
@@ -248,6 +356,12 @@ class NoteBag:
         self.open_note(note_name)
 
     def delete_note_from_listbox(self, *_args, **_kwargs):
+        """
+        If a note name has been selected in the note name list, delete
+        it after prompting the user to confirm; otherwise, show a
+        warning dialog box and do nothing.
+        """
+
         note_name = self.get_listbox_selected_note_name()
         if not note_name:
             messagebox.showwarning("Error", "Can't Delete: No note selected")
@@ -336,6 +450,11 @@ class NoteBag:
         self.update_note_names_list()
 
 def maybe_first_time_setup():
+    """
+    Set up the user's notes directory/folder the first time they run
+    NoteBag.
+    """
+
     config = read_config(CONFIG_FILENAME)
     if config.get("NoteBag", "Notes Directory"):
        return True
